a8f19daeb6a387e47984426e08019644b6682dd8
pankajsharma <pankaj.sharma0135@gmail.com>
2019-06-27 11:19:50 +0530

    Uploading project on GitHub
    Uploading project on GitHub

---
+++ FirstServlet/src/com/journaldev/first/FirstServlet.java
@@ -0,0 +1,50 @@
+package com.journaldev.first;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet(description = "FirstServlet", urlPatterns = { "/FirstServlet","/FirstServlet.do" },initParams={@WebInitParam(name="id",value="1"),@WebInitParam(name="name",value="pankaj")})
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String HTML_START="<html><body>";
+	public static final String HTML_END="</body></html>";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		Date date = new Date(); 
+		out.println(HTML_START+"<h3>Hi There!</h3><br/>Today's date is "+date+HTML_END);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
